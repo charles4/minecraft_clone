0afdc7732183d16df4c0f2eab83b3ed73093cfae
charles <charles4@email.arizona.edu>
2013-06-25 13:40:10 -0700

    more webgl learnin`

--- server.py
+++ server.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template, session, redirect, url_for, abort, request, flash, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def route_base():
-	return render_template("home.html")
+@app.route("/1", methods=['GET'])
+def route_1():
+	return render_template("1.html")
+
+@app.route('/2', methods=['GET'])
+def route_2():
+	return render_template("2.html")
+
+@app.route('/3', methods=['GET'])
+def rotue_3():
+	return render_template("3.html")
+
+@app.route('/4', methods=['GET'])
+def route_4():
+	return render_template("4.html")
 
 if __name__ == "__main__":
 	app.debug = True
